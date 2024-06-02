5c87d7dc6e083ac4414bd493e438cf3a54052d00
Masaood Shah <masaoodshah803@gmail.com>
2024-06-03 00:54:31 +0500

    new files

---
+++ script.js
@@ -0,0 +1,104 @@
+// 01 Barish---------------
+
+// var clouds = "yes"
+// var noClouds = "no"
+// var rain = prompt("Is There Any Clouds? (Awnser in yes or no)")
+
+// if(rain === clouds){
+//     console.log("There is Chances of Rain")
+// }
+// else if(rain === noClouds){
+//     console.log("There is No Chances of Rain")
+// } 
+
+// 02 BRT Program---------------
+
+// var userBalance = +prompt("Enter your balance")
+
+// if(userBalance >= 50){
+//     alert("Welcome To ZU Peshawar")
+// }
+
+// if(userBalance < 50){
+//     alert("Please recharge your balance")
+// }
+
+// 03 Traffic Light System-------------------
+
+// var input = prompt("Please Enter Any Traffic Light Color In Lower Case")
+
+// if(input == "red"){
+//     alert("Stop")
+// }
+// else if(input == "yellow"){
+//     alert("Get Ready")
+// }
+// else if(input == "green"){
+//     alert("Go")
+// }
+// else{
+//     alert("Invalid Color")
+// }
+
+// 04 Shopping Discount----------
+
+// var amount = +prompt("Enter Shopping Amount")
+
+// if(amount <= 4999){
+//     alert("You Got 10% Discount")
+// }
+// else if(amount <= 9999){
+//     alert("You Got 25% Discount")
+// }
+// else if(amount >= 10000){
+//     alert("You Got 50% Discount")
+// }
+
+// 05 ATM------------
+
+// var balance = 5000
+// var withdraw = +prompt("Enter Amount To Withdraw")
+
+// if(withdraw <= balance){
+//     console.log("Please Take Your Money From ATM Machine")
+// }
+// else if(withdraw >= balance){
+//     console.log("You Have Insufficient Balance In your Account")
+// }
+
+// 06 Even Odd-----------------
+
+// var number = +prompt("Enter Number")
+
+// if(number % 2 === 0){
+//     console.log("Number is Even")
+// }
+// else{
+//     console.log("Number is Odd")
+// }
+
+// 07 Total Bill-----------
+
+// var books = +prompt("Enter total amount of books")
+// var noteBooks = +prompt("Enter total amount of notebooks")
+// var pens = +prompt("Enter total amount og pens")
+
+// var total = books + noteBooks + pens
+
+// console.log(`Total Bill
+//              Books = ${books}
+//              Notebook = ${noteBooks}
+//              Pens = ${pens}
+//              Total Amount = ${total}`)
+
+// 08 Burger----------
+
+// var burgerPrice = 350
+// var pocketMoney = +prompt("Paise Kitne Hain Bhai")
+
+// if(pocketMoney >= burgerPrice){
+//     console.log("Your Burger Is Getting Ready")
+// }
+// else{
+//     console.log("Sorry! You Dont't Have Enough Money")
+// }
